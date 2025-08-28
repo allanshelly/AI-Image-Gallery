e553169051c6ef39b0dd145c8abf7e774ceb2a8a
allanshelly <allansheldon07@gmail.com>
2025-08-28 23:11:58 +0800

    fixed logged out on home not showing proper router link

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.html',
   styleUrls: ['./home.scss'],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class HomeComponent {
   constructor(public auth: AuthService, private router: Router) {}
